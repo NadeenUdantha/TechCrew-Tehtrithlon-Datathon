9e35fed02f78ade5cd1d2aa39234af1c57cbc45e
Nadeen Udantha <udanthan@gmail.com>
2024-09-14 11:20:55 +0530

    review_scores data

--- review_score.py
+++ review_score.py
@@ -1,9 +1,10 @@
+from concurrent.futures import ThreadPoolExecutor
 import tqdm
 import csv
 import os
-from joblib import Memory
 import requests
 import dotenv
+from joblib import Memory
 
 dotenv.load_dotenv()
 
@@ -11,12 +12,15 @@ API_BASE_URL = os.environ['API_BASE_URL']
 headers = {'Authorization': 'Bearer '+os.environ['API_TOKEN']}
 
 
+session = requests.session()
+
+
 def run(model, input):
-    response = requests.post(f'{API_BASE_URL}{model}', headers=headers, json=input)
+    response = session.post(f'{API_BASE_URL}{model}', headers=headers, json=input)
     return response.json()
 
 
-memory = Memory('./cache', verbose=0)
+memory = Memory('./cache')
 
 
 @memory.cache
@@ -33,13 +37,25 @@ def process_reviews(input_csv, output_csv):
 
     with open(input_csv, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in tqdm.tqdm(list(reader)):
+        rows = list(reader)
+        for row in rows:
+            row['latest_reviews'] = row['latest_reviews'].removeprefix("['").removesuffix("']").split("', '")
+        pb = tqdm.tqdm(total=sum([len(row['latest_reviews']) for row in rows]))
+        pool = ThreadPoolExecutor(max_workers=10)
+
+        def f(row):
             name = row['name']
-            reviews = row['latest_reviews'].removeprefix("['").removesuffix("']").split("', '")
+            reviews = row['latest_reviews']
 
-            scores = [get_score(review) for review in reviews]
+            scores = []
+            for review in reviews:
+                scores.append(get_score(review))
+                pb.update(1)
 
-            results.append([name, scores])
+            return [name, scores]
+
+        for x in pool.map(f, rows):
+            results.append(x)
 
     with open(output_csv, mode='w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
